fd7982e97c4942db5e05b029be965c3c857b1698
Amit Evron <amit3vr@gmail.com>
2015-08-21 05:01:16 +0300

    needs a rewrite

--- lang/he-il/errors.lang.php
+++ lang/he-il/errors.lang.php
@@ -2,13 +2,5 @@
 
 return [
 
-    'module_not_init'
-        => "מודול לא מאותחל במערכת.",
-
-    'module_file_not_found'
-        => "קובץ מודול לא נמצא.",
-
-    'module_not_unified'
-        => "מודול לא שומר על חוקי מערכת.",
 
 ];
