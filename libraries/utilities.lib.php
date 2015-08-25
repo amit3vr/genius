0ef1328afc182bee2215a68695f7d46cf332e6b7
Amit Evron <amit3vr@gmail.com>
2015-08-25 10:23:29 +0300

    init function added for convinient way to initialize a variable

--- libraries/utilities.lib.php
+++ libraries/utilities.lib.php
@@ -6,4 +6,10 @@ final class Utilities
     {
         return (microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]);
     }
+
+    public static function init(&$var, $value)
+    {
+        if(!isset($var) || empty($var))
+            $var = $value;
+    }
 }
