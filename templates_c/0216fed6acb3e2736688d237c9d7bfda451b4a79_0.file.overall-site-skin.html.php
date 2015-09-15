5d5a83b28b136c13f7e06f0c63f2fadeca9aba3e
amit3vr <amit3vr@gmail.com>
2015-09-15 17:09:17 +0300

    .gitignore added for unnecessery backups

--- templates_c/0216fed6acb3e2736688d237c9d7bfda451b4a79_0.file.overall-site-skin.html.php
+++ templates_c/0216fed6acb3e2736688d237c9d7bfda451b4a79_0.file.overall-site-skin.html.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.27, created on 2015-09-05 03:57:50
+<?php /* Smarty version 3.1.27, created on 2015-09-15 12:12:49
          compiled from "C:\xampp\htdocs\genius\template\overall-site-skin.html" */ ?>
 <?php
-/*%%SmartyHeaderCode:1384655ea3e0ea18715_77519305%%*/
+/*%%SmartyHeaderCode:1518555f7e111444658_83641174%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,55 +9,79 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '0216fed6acb3e2736688d237c9d7bfda451b4a79' => 
     array (
       0 => 'C:\\xampp\\htdocs\\genius\\template\\overall-site-skin.html',
-      1 => 1441414275,
+      1 => 1442308254,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '1384655ea3e0ea18715_77519305',
+  'nocache_hash' => '1518555f7e111444658_83641174',
   'variables' => 
   array (
+    'site' => 0,
     'title' => 0,
+    'user' => 0,
     'content' => 0,
-    'exec_timer' => 0,
   ),
   'has_nocache_code' => false,
   'version' => '3.1.27',
-  'unifunc' => 'content_55ea3e0ea5ee18_11802081',
+  'unifunc' => 'content_55f7e11148a2f6_42892997',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_55ea3e0ea5ee18_11802081')) {
-function content_55ea3e0ea5ee18_11802081 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_55f7e11148a2f6_42892997')) {
+function content_55f7e11148a2f6_42892997 ($_smarty_tpl) {
 
-$_smarty_tpl->properties['nocache_hash'] = '1384655ea3e0ea18715_77519305';
+$_smarty_tpl->properties['nocache_hash'] = '1518555f7e111444658_83641174';
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 <head>
-<title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+<title><?php echo $_smarty_tpl->tpl_vars['site']->value['name'];?>
+ > <?php echo $_smarty_tpl->tpl_vars['title']->value;?>
 </title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:400,300|Slabo+27px|Lora|Bitter|PT+Serif|Bree+Serif|Sanchez|Roboto:400,300">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="resources/bootstrap3/css/bootstrap.min.css">
 <?php echo '<script'; ?>
- src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"><?php echo '</script'; ?>
+ src="resources/bootstrap3/js/bootstrap.min.js"><?php echo '</script'; ?>
 >
-<?php echo '<script'; ?>
- src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"><?php echo '</script'; ?>
->
+<link rel="stylesheet" type="text/css" href="resources/site.css">
+<link rel="stylesheet" type="text/css" href="resources/feed.css">
 </head>
+
+<nav>
+    <ul>
+        <li class="logo"><?php echo $_smarty_tpl->tpl_vars['site']->value['name'];?>
+</li>
+        <li>
+            <span class="seperator"></span>
+        </li>
+        <li>
+            <a class="fa fa-globe fa-lg" href="#"></a>
+        </li>
+        <li>
+            <a class="fa fa-envelope fa-lg" href="#"></a>
+        </li>
+    </ul>
+    <ul>
+        <?php if ($_smarty_tpl->tpl_vars['user']->value) {?>
+        <li class="profile-details">
+            <div><a href="#">Amit Evron</a></div>
+            <div>Teacher</div>
+        </li>
+        <li>
+            <img src="http://bootstrap-themes.github.io/application/assets/img/avatar-dhg.png" class="profile-pic" />
+        </li>
+        <?php } else { ?>
+        <li class="profile-details">
+            Guest, Log-in
+        </li>
+        <?php }?>
+    </ul>
+</nav>
+
 <body>
-
-<div class="container">
-    <div class="jumbotron">
-        <h1><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
-</h1>
-        <p><?php echo $_smarty_tpl->tpl_vars['content']->value;?>
-</p>
-    </div>
-
-    <div>Execution time: <?php echo $_smarty_tpl->tpl_vars['exec_timer']->value;?>
- second(s)</div>
-</div>
+<?php echo $_smarty_tpl->tpl_vars['content']->value;?>
 
 </body>
 </html>
--- templates_c/0a8fe37c6c193981e8a18375ce918c59ffb21abf_0.file.login.html.php
+++ templates_c/0a8fe37c6c193981e8a18375ce918c59ffb21abf_0.file.login.html.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.27, created on 2015-09-05 03:12:21
+<?php /* Smarty version 3.1.27, created on 2015-09-15 16:39:02
          compiled from "C:\xampp\htdocs\genius\template\login.html" */ ?>
 <?php
-/*%%SmartyHeaderCode:786755ea3365d90911_97083642%%*/
+/*%%SmartyHeaderCode:534155f81f76c1e239_40577547%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,37 +9,70 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '0a8fe37c6c193981e8a18375ce918c59ffb21abf' => 
     array (
       0 => 'C:\\xampp\\htdocs\\genius\\template\\login.html',
-      1 => 1441406549,
+      1 => 1441989883,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '786755ea3365d90911_97083642',
-  'variables' => 
-  array (
-    'error' => 0,
-  ),
+  'nocache_hash' => '534155f81f76c1e239_40577547',
   'has_nocache_code' => false,
   'version' => '3.1.27',
-  'unifunc' => 'content_55ea3365dcdbd5_42163165',
+  'unifunc' => 'content_55f81f76c92636_40171104',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_55ea3365dcdbd5_42163165')) {
-function content_55ea3365dcdbd5_42163165 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_55f81f76c92636_40171104')) {
+function content_55f81f76c92636_40171104 ($_smarty_tpl) {
 
-$_smarty_tpl->properties['nocache_hash'] = '786755ea3365d90911_97083642';
-if ($_smarty_tpl->tpl_vars['error']->value) {?>
-<p>One of the fields key\password is invalid.</p>
-<?php }?>
-<form role="form" method="post">
-    <div class="form-group">
-        <label for="key">Key:</label>
-        <input name="key" class="form-control" id="key" placeholder="Enter email/username/id">
+$_smarty_tpl->properties['nocache_hash'] = '534155f81f76c1e239_40577547';
+?>
+<style>
+.login-page>div {
+    text-align: center;
+    padding: 90px 0;
+}
+.login-page>form {
+    width: 450px;
+    margin: auto;
+}
+.login-page>form>div {
+    padding-bottom: 10px;
+}
+.login-page h1 {
+    margin-top: 20px;
+
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    font-size: 72px;
+}
+.login-page h6 {
+    font-family: 'Lora', sans-serif;
+    font-weight: 300;
+    font-size: 18px;
+    color: rgba(149, 165, 166, 1);
+}
+</style>
+
+<div class="login-page">
+    <div>
+        <h1 style="padding-bottom: 10px">Login to Genius</h1>
+        <h6>Keep updated. Track your scores. Break your limits.</h6>
     </div>
-    <div class="form-group">
-        <label for="password">Password:</label>
-        <input name="password" type="password" class="form-control" id="password" placeholder="Enter password">
-    </div>
-    <button type="submit" class="btn btn-default">Submit</button>
-</form><?php }
+
+    <form method="post" action="#">
+        <div class="input-group">
+            <span class="input-group-addon fa fa-user" aria-hidden="true"></span>
+            <input type="text" name="key" class="form-control" placeholder="Email address or ID" autocomplete="off">
+        </div>
+
+        <div class="input-group">
+            <span class="input-group-addon fa fa-lock" aria-hidden="true"></span>
+            <input type="password" name="password" class="form-control" placeholder="Password" autocomplete="off">
+        </div>
+
+        <div>
+            <div class="pull-left"><a href="#" style="font-size: 11px">forgot your password?</a></div>
+            <div class="pull-right"><button type="submit" class="btn btn-primary">GO >></button></div>
+        </div>
+    </form>
+</div><?php }
 }
 ?>
